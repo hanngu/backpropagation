427773a799c3591b6a6dcc0732377ac71a40336a
Hanne Gunby <hanngu@saltsyre.local>
2014-03-31 20:55:59 +0200

    order pairs and keep training performance

--- dataLoaderSkeleton.py
+++ dataLoaderSkeleton.py
@@ -60,26 +60,50 @@ def runRanker(trainingset, testset):
     for qid in dhTraining.dataset.keys():
         #This iterates through every query ID in our training set
         dataInstance=dhTraining.dataset[qid] #All data instances (query, features, rating) for query qid
+
         #TODO: Store the training instances into the trainingPatterns array. Remember to store them as pairs, where the first item is rated higher than the second.
         #TODO: Hint: A good first step to get the pair ordering right, is to sort the instances based on their rating for this query. (sort by x.rating for each x in dataInstance)
+
+        uniquely_rated_instances = []
+        for instance in dataInstance:
+            ratings_seen = [x.rating for x in uniquely_rated_instances]
+            if instance.rating not in ratings_seen:
+                uniquely_rated_instances.append(instance)
+
+        sorted_instances = sorted(uniquely_rated_instances)
+        for i in sorted_instances:
+            for j in sorted_instances:
+                if i.rating > j.rating:
+                    trainingPatterns.append((i, j),)
 
     for qid in dhTesting.dataset.keys():
         #This iterates through every query ID in our test set
         dataInstance=dhTesting.dataset[qid]
         #TODO: Store the test instances into the testPatterns array, once again as pairs.
         #TODO: Hint: The testing will be easier for you if you also now order the pairs - it will make it easy to see if the ANN agrees with your ordering.
+        uniquely_rated_instances = []
+        for instance in dataInstance:
+            ratings_seen = [x.rating for x in uniquely_rated_instances]
+            if instance.rating not in ratings_seen:
+                uniquely_rated_instances.append(instance)
+
+        sorted_instances = sorted(uniquely_rated_instances)
+        for i in sorted_instances:
+            for j in sorted_instances:
+                if i.rating > j.rating:
+                    testPatterns.append((i, j),)
 
     #Check ANN performance before training
-    nn.countMisorderedPairs(testPatterns)
+    training_performance = []
+    training_performance.append(nn.countMisorderedPairs(testPatterns))
     for i in range(25):
         #Running 25 iterations, measuring testing performance after each round of training.
         #Training
-        nn.train(trainingPatterns,iterations=1)
+        nn.train(trainingPatterns, iterations=1)
         #Check ANN performance after training.
-        nn.countMisorderedPairs(testPatterns)
+        training_performance.append(nn.countMisorderedPairs(testPatterns))
 
     #TODO: Store the data returned by countMisorderedPairs and plot it, showing how training and testing errors develop.
-
 
 
 runRanker("train.txt","test.txt")
